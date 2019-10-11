71b3945b2694588905e9843dcd3b0cf2e7128b21
BranceLee <475815059@qq.com>
2019-10-11 17:01:31 +0800

    add device, image, faceimage, event table

--- sql_07/server.sql
+++ sql_07/server.sql
@@ -1,9 +1,23 @@
--- Drop database testdb;
--- Create database testdb;
+Drop database testdb;
+Create database testdb;
 \c testdb;
 
-Create Table public.User  (
-    UserID                  int             primary key     not null    unique,
+Create Table VmqAuthACL (
+    Mountpoint              varchar(10)     not null,
+    ClientID                varchar(128)    not null    unique,
+    Username                varchar(128)    not null    unique,
+    PublishACL              json,
+    SubscribeACL            json,
+    primary key(Mountpoint, ClientID, Username)
+);
+
+Create Table SN (
+    SNID                    int             primary key,
+    Value                   varchar(255)    unique
+);
+
+Create Table Users  (
+    UserID                  int             primary key,
     UUID                    uuid            not null,
     Email                   varchar(255),
     Password                varchar(255)    ,
@@ -15,12 +29,15 @@ Create Table public.User  (
     MQTTClientID            varchar(128),
     AllowPushNotification   boolean          default false,
     EnableTwoFactorLogin   boolean          default true,
-       
+    CreatedAt               timestamp        not null,     
+    UpdatedAt               timestamp       not null,
+    DeletedAt               timestamp        not null,
+
     Foreign key(MQTTClientID) References VmqAuthACL(ClientID)
 );
 
 Create Table Device (
-    DeviceID                int             primary key     not null,
+    DeviceID                int             primary key,
     Kind                    int             not null,
     Owner                   int,
     Token                   varchar(255),
@@ -28,28 +45,132 @@ Create Table Device (
     MQTTClientID            varchar(128),
     Version                 varchar(128)    not null,
     DID                     varchar(255)    not null,
-    Foreign key (SN) References SN(SNID),
+    CreatedAt               timestamp        not null,     
+    UpdatedAt               timestamp        not null,
+    DeletedAt               timestamp        not null,
+
+    Foreign key (SN) References SN(Value),
     Foreign key (MQTTClientID) References VmqAuthACL(ClientID)
 );
 
-Create Table SN (
-    SNID                    int             primary key,
-    Value                   varchar(255)
-);
-
 Create Table DeviceUserShare (
-    ID                      int             primary key,
+    ID                      int             primary key ,
     DeviceSN                varchar(255)    unique,
-    UserID                  int             unique,
+    UserID                  int             unique  not null,
     Status                  varchar(255),
-    Foreign key (UserID) References User(UserID)
+    Foreign key (UserID) References Users(UserID),
+    Foreign key (DeviceSN) References Device(SN)
 );
 
-Create Table VmqAuthACL (
-    Mountpoint              varchar(10)     not null,
-    ClientID                varchar(128)    not null    unique,
-    Username                varchar(128)    not null    unique,
-    PublishACL              json,
-    SubscribeACL            json,
-    primary key(Mountpoint, ClientID, Username)
+Create Table DeviceSetting (
+    DeviceSettingID         int             primary key not null,
+    UUID                    uuid            unique  not null,
+    UserID                  int             unique  not null,
+    DeviceSN                varchar(255)    unique  not null,
+    DeviceName              varchar(255)    not null,
+    AllowMotionNotification  boolean         not null,
+    AllowRingNotification    boolean         not null,
+    DoorbellVolume          int             not null,
+    ChimeVolume             int             not null,
+    AllowFacialRecognition  boolean         not null,
+    EnableLight             boolean         not null,
+    EnableAntiTheftAlert    boolean         not null,
+    CreatedAt               timestamp       not null,     
+    UpdatedAt               timestamp       not null,
+    DeletedAt               timestamp       not null,
+
+    Foreign key (UserID) References Users(UserID),
+    Foreign key (DeviceSN) References Device(SN)
 );
+
+Create Table Media(
+    MediaID                 int             primary key,
+    UserID                  int             not null unique,
+    Type                    int             not null,
+    S3Key                   varchar(255)    not null,
+    S3Bucket                varchar(255)    not null,
+    CreatedAt               timestamp       not null,     
+    UpdatedAt               timestamp       not null,
+    DeletedAt               timestamp       not null,
+
+    Foreign key (UserID) References Users(UserID)
+);
+  
+Create Table VideoThumbnail(
+    VideoThumbnailID        int             primary key,
+    MediaID                 int             not null,
+    S3Key                   varchar(255)    not null,
+    S3Bucket                varchar(255)    not null,
+    Width                   int             not null,
+    Height                  int             not null,
+    CreatedAt               timestamp       not null,     
+    UpdatedAt               timestamp       not null,
+    DeletedAt               timestamp       not null,
+
+    Foreign key (MediaID) References Media(MediaID)
+);
+
+Create Table VideoStream(
+    VideoStreamID           int             primary key,
+    MediaID                 int             not null,
+    S3Key                   varchar(255)    not null,
+    S3Bucket                varchar(255)    not null,
+    Width                   int             not null,
+    Height                  int             not null,
+    Bitrate                 int             not null,
+    CreatedAt               timestamp       not null,     
+    UpdatedAt               timestamp       not null,
+    DeletedAt               timestamp       not null,
+
+    Foreign key (MediaID) References Media(MediaID)
+);
+
+Create Table Event (
+    EventID                 int             primary key,
+    UUID                    uuid            not null,
+    UserID                  int             not null,
+    MediaID                 int             not null,
+    Kind                    int             not null,
+    Timestamp               bigint          not null,
+    Filename                varchar(255)    not null,
+    DeviceSN                varchar(255)    not null,
+    Duration                int             not null,
+    CreatedAt               timestamp       not null,     
+    UpdatedAt               timestamp       not null,
+    DeletedAt               timestamp       not null,
+
+    Foreign key (UserID) References Users(UserID),
+    Foreign key (DeviceSN) References Device(SN),
+    Foreign key (MediaID) References Media(MediaID)
+);
+
+Create Table FaceProfile(
+    FaceProfileID            int             primary key,
+    Name                    varchar(255)    not null,
+    Tags                    varchar(255)    not null,
+    UUID                    uuid            not null unique,
+
+    CreatedAt               timestamp       not null,     
+    UpdatedAt               timestamp       not null,
+    DeletedAt               timestamp       not null
+);
+
+Create Table FaceImage(
+    FaceImageID             int             primary key,
+    Owner                   int             not null,
+    DeviceSN                varchar(255)    not null,
+    FaceID                  uuid            not null unique, 
+    EventID                 int             not null,
+    FaceProfileID            int,
+    S3Bucket                varchar(255)    not null,
+    S3Key                   varchar(255)    not null,
+
+    CreatedAt               timestamp       not null,     
+    UpdatedAt               timestamp       not null,
+    DeletedAt               timestamp       not null,
+
+    Foreign key(DeviceSN) References Device(SN),
+    Foreign key(EventID) References Event(EventID),
+    Foreign key(FaceProfileID) References FaceImage(FaceImageID)
+);
+
